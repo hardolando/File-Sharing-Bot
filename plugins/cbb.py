fefd36b04ab6465e321495f4c5b5e823d8b87ea0
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2021-08-25 15:54:59 +0700

    asv

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = "WELCOME TO A§V GROUP\nHere is the main list of A§V CHANNEL AND GROUP :\n○ MAIN CHANNEL : <a href='https://t.me/asupanviralid'>A§ID</a>\n○ RARE CONTENT : <a href='https://t.me/asupanviralid2'>A§ID2</a>\n○ UNDERAGES : <a href='https://t.me/asupanviralbovchil'>A§ID BOVCHL</a>\n\nOFFICIAL A§V GROUP :/n/n○ MAIN GROUP CHAT : <a href='https://t.me/INFOLINKASV'>GROUP</a>\n.\n○ CONTACT PERSON/INFO VVIP : <a href='https://t.me/asvservicecenter'>ADMIN A§V</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("Iya Paham", callback_data = "close")
                     ]
                 ]
             )
