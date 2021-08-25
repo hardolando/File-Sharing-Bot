156b2ee1048d3c519adc3fa8c933d410da1eb1ca
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2021-08-25 16:14:06 +0700

    FORCE SUB

--- plugins/start.py
+++ plugins/start.py
@@ -40,11 +40,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("bentar bos...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("Aduh ada yang salah")
             return
         await temp_msg.delete()
 
@@ -73,8 +73,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("OFFICIAL A§V INFO", callback_data = "about"),
+                    InlineKeyboardButton("Hmm.. Oke", callback_data = "close")
                 ]
             ]
         )
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>Join dulu ke channel nya biar bisa ngintip isinya, Lu masuk channel auto gua kasih filenya bos..</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>Kalo Udah <a href='https://t.me/{client.username}?start={argument}'>KLIK DISINI</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Sini Bos..", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
