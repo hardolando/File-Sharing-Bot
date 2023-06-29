5f729956fd8647dd6512ce25eb10b50d77e5ccc8
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2023-06-29 22:23:09 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,19 +8,19 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
+    if data == "GW":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n Channel : @asupanviralid\n Support Group : @infolinkasv</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("dahlah", callback_data = "dah")
                     ]
                 ]
             )
         )
-    elif data == "close":
+    elif data == "dah":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
